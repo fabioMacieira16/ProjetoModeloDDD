b6fd5acc749db6108d0dbe90ef8e3e7028a4600b
Fabio Macieira <fabio.macieira@solucaosistemas.net>
2020-02-07 17:06:17 -0300

    Organização do projeto e criação do modelo Cliente

---
+++ ProjetoModeloDDD.Infra.Data/Contexto/ProjetoModelContexto.cs
@@ -0,0 +1,16 @@
+﻿using ProjetoModeloDDD.domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ProjetoModeloDDD.Infra.Data.Contexto
+{
+    public class ProjetoModelContexto : DbContext
+    {
+        public ProjetoModelContexto()
+            : base("ProjetoModeloDDD")
+        {
+
+        }
+       public DbSet <Cliente> Clientes { get; set; }
+    }
+}
---
+++ ProjetoModeloDDD.domain/Entities/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjetoModeloDDD.domain.Entities
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        public bool ClienteEspecial(Cliente cliente) 
+        {
+            //verifica se o cliente esta ativo e se tem mais de 5 anos de cadastro
+            return cliente.Ativo && DateTime.Now.Year - cliente.DataCadastro.Year >= 5;
+        }
+    }
+}
